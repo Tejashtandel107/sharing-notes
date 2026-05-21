12ad84dc2749868cc715a4dafa0a1796d77c17e5
tejashtandel <tejas@itidoltechnologies.com>
2026-05-21 15:20:21 +0530

    Deploy setup

--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
   host: '0.0.0.0',
   port: env.int('PORT', 1337),
+
+  url: env('PUBLIC_URL'),
+
+  proxy: true,
+
   app: {
     keys: env.array('APP_KEYS'),
   },
 });
-
-
